1e7045d491d458f9562dcebf3b0cf3a89d0037b9
sagecat <1070652242@qq.com>
2021-11-28 20:29:01 +0400

    init the project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import basic.syntax.Shape;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+    }
+}
---
+++ src/basic/syntax/Shape.java
@@ -0,0 +1,21 @@
+package basic.syntax;
+
+public class Shape
+{
+    private String name;
+
+    public Shape()
+    {
+    }
+
+    public Shape(String name)
+    {
+        this.name = name;
+    }
+
+    public double area()
+    {
+        System.out.println("Please implement the method of the area");
+        return 0.0;
+    }
+}
