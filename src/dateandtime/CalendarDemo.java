dd5b22277e983685e8afc8339a476c7f9c806f00
sagecat <1070652242@qq.com>
2021-12-03 00:12:42 +0400

    learn Calendar

---
+++ src/dateandtime/CalendarDemo.java
@@ -0,0 +1,36 @@
+package dateandtime;
+
+import java.util.Calendar;
+
+public class CalendarDemo
+{
+    public static void main(String[] args)
+    {
+        /*
+        Calendar 代替了 Date 类中的很多功能
+        Calendar 是一个抽象类，提供了很多操作日历字段的方法
+        由于是抽象类，无法直接创建对象，可以通过 getInstance 获取一个 Calendar的子类GregorianCalendar对象
+         */
+        Calendar instance = Calendar.getInstance();
+        System.out.println(instance);
+
+        /*
+        常用成员方法：
+        1. get(int type) 获取方法，可获取年份，月份，周等等
+        2. set(int type) 获取方法，可获取年份，月份，周等等
+        3. add(int type) 计算 年月日偏移量等等
+        4. getTime() 把 Calendar对象转换为Date对象
+         */
+
+        // 获取年份
+        System.out.println(instance.get(Calendar.YEAR));
+        // 获取月份
+        System.out.println(instance.get(Calendar.MONTH)+1);
+        // 获取当月的第几天
+        System.out.println(instance.get(Calendar.DAY_OF_MONTH));
+        // 获取24小时制 的几点
+        System.out.println(instance.get(Calendar.HOUR_OF_DAY));
+        // 获取12小时制 的几点
+        System.out.println(instance.get(Calendar.HOUR));
+    }
+}
--- src/dateandtime/DateFormatDemo.java
+++ src/dateandtime/DateFormatDemo.java
@@ -2,7 +2,9 @@ package dateandtime;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 public class DateFormatDemo
 {
@@ -29,5 +31,27 @@ public class DateFormatDemo
          */
         Date dateOfString = simpleDateFormat.parse("2021-11-22");
         System.out.println(dateOfString);
+
+
+        /*
+        练习：获取一个人出生的天数
+            1. 通过Scanner类中的方法 next, 获取一个人的出生日期，格式是 字符串
+            2. 将字符串转化为 日期格式，使用Dateformate的parse方法
+            3. 把日期转换为毫秒值
+            4. 获取当前日期并转换为毫秒值
+            5. 计算上述2个毫秒值的差值
+            6. 将毫秒值转化为 天数
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入您的出生日期： yyyy-MM-dd");
+        String dateString = scanner.next();
+        SimpleDateFormat birthdayFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date birthday = birthdayFormat.parse(dateString);
+        long birthdayMills = birthday.getTime();
+        long currentMills = System.currentTimeMillis();
+
+        long millsDiff = currentMills - birthdayMills;
+        System.out.println(millsDiff / 1000 / 3600 / 24);
     }
 }
